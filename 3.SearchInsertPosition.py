c77c002049cd67224d860ce126bc567eaa69642a
vishalkscoder-dev <vishalks.coder@gmail.com>
2026-07-04 23:00:41 +0530

    Add files via upload

---
+++ 3.SearchInsertPosition.py
@@ -0,0 +1,27 @@
+import os
+os.system('cls')
+
+
+def searchInsert(arr, target):
+    left = 0
+    right = len(arr)-1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid
+        
+        elif arr[mid] < target:
+            left = mid + 1
+        
+        else:
+            right = mid - 1
+
+        return left
+
+arr = [1,2,4,7,37]
+target = 5
+
+print(searchInsert(arr, target))
+
