381085a1d83f230230e2754f7a2879c3fcd3a7b8
vishalkscoder-dev <vishalks.coder@gmail.com>
2026-08-18 21:54:10 +0530

    Add files via upload

---
+++ 43.h-Index.py
@@ -0,0 +1,15 @@
+import os
+os.system('cls')
+
+def hIndex(self, citations):
+    citations.sort(reverse=True)
+
+    h = 0
+
+    for i in range(len(citations)):
+        if citations[i] >= i+1:
+            h = i + 1
+        else:
+            break
+
+    return h
