5a65d8b0ea586a7834ed56ddcdd209e3c300017c
vishalkscoder-dev <vishalks.coder@gmail.com>
2026-08-30 00:15:58 +0530

    Add files via upload

---
+++ 54.linkedListCycle.py
@@ -0,0 +1,38 @@
+import os
+os.system('cls')
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def hasCycle(head):
+    slow = head
+    fast = head
+
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+        if slow == fast:
+            return True
+
+    return False
+
+
+node1 = ListNode(1)
+node2 = ListNode(2)
+node3 = ListNode(3)
+node4 = ListNode(4)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+
+node4.next = node2
+
+head = node1
+
+print(hasCycle(head))
+
+
